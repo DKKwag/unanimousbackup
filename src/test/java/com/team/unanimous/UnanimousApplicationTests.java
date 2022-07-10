6601a6a3d670e53768fc667670534f33006823fd
DKKwag <105155862+DKKwag@users.noreply.github.com>
2022-07-11 05:52:20 +0900

    dd

--- src/test/java/com/team/unanimous/UnanimousApplicationTests.java
+++ src/test/java/com/team/unanimous/UnanimousApplicationTests.java
@@ -3,11 +3,7 @@ package com.team.unanimous;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class UnanimousApplicationTests {
-    @Test
 
-    public void test () {
-
-    }
 }
